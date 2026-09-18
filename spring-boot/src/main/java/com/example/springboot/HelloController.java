12779a5bf2a4373c02aa3134d05d2adb08f941d2
Riordin Dupree <riordindupree@u.boisestate.edu>
2026-09-18 15:04:46 -0600

    set up github and added basic SpringBoot page.

---
+++ spring-boot/src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/")
+  public String index() {
+    return "<h5 class=\"text text-center text-success\">Hello World!</h5>";
+  }
+
+}
